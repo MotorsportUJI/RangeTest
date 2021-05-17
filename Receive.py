19e73aca7a60692ed54f68be353025de8e626830
vicdeaj <75012352+vicdeaj@users.noreply.github.com>
2021-05-17 13:51:17 +0200

    Update Receive.py

--- Receive.py
+++ Receive.py
@@ -6,19 +6,27 @@ buffer = bytes()
 anterior = 0
 error = 0
 contador = 0
-s = serial.Serial("/dev/ttyUSB0")
+s = serial.Serial("/dev/serial0")
 s.baudrate = 115200
 
 time0 = time.time()
+s.reset_input_buffer()
 while True:
-    if s.in_waiting >= 64:
-        buffer = s.read(s.in_waiting)
-        data = buffer[0:60]
-        crc = buffer[60:65]
+#    print(s.in_waiting)
+    if s.in_waiting >= 58:
+        buffer = s.read(58)
+#        print(buffer)
+        data = buffer[:50]
+        foreign_counter = buffer[50:54]
+        foreign_counter_int = int.from_bytes(foreign_counter,"little")
+        crc = buffer[54:]
 
-        guess = crc32(data).to_bytes(4, "little")
+        guess = crc32(data+foreign_counter).to_bytes(4, "little")
         if guess == crc: # no se ha liao
             print("{}: \tMensaje recibido correctamente".format(contador))
+            if foreign_counter_int != contador:
+                print("PAQUTES PERDIDOS: {}".format(foreign_counter_int-contador))
+                contador = foreign_counter_int
         else:
             print("{}: \tERROR ERROR".format(contador))
         time0 = time.time()
@@ -29,7 +37,7 @@ while True:
         pasado = time.time()-time0
         if s.in_waiting == 0:
             time0 = time.time() # resetear para que el timeout no salte cuando no se esta enviando nada (esto puede que sea un poquito pochingui)
-        if pasado > 0.2:
+        if pasado > 0.5:
             print("{}: \tTIMEOUT TIMEOUT".format(contador))
             s.reset_input_buffer()
             time0 = time.time()
